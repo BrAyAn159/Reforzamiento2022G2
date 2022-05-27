b6e787d94710d96c0e3f963b715038adafa3c3a2
BrAyAn159 <cornejob797@gmail.com>
2022-05-27 20:46:40 +0000

    trabajo

---
+++ ref3g/RefRepetitivas.py
@@ -0,0 +1,15 @@
+def serieFibonaci():
+    nI:int(0)
+    ns=1
+    n=int(impult("Introduce el N numero:"))
+    cont=1
+    while(cont<n):
+        print(nS)
+        nT=nS+nI
+        nI=nS
+        nS=nT
+        cont=cont+1
+
+
+
+serieFibonaci()        
