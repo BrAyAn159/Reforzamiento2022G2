4b3715b50a6af3edeb394e8577343ec779883f63
BrAyAn159 <cornejob797@gmail.com>
2022-05-27 20:19:39 +0000

    trabajoR

---
+++ ref3g/RefRepetitivas.java
@@ -0,0 +1,30 @@
+package ref3g;
+
+import java.util.Scanner;
+
+/**
+ * RefRepetitivas
+ */
+public class RefRepetitivas {
+    static Scanner leerT=new Scanner(System.in);
+    //Pregunta 01 Reforzamiento
+    public static void serieFibonaci() {
+        int nI=0, nS=1, nT;
+        System.out.println("Introduce el N numero:");
+        int n=leerT.nextInt();
+        System.out.println(nI);
+        int cont=1;
+        while (cont<n) {  
+            System.out.println(nS);              
+            nT=nS+nI;                        
+            nI=nS;
+            nS=nT;
+            cont++;            
+        }        
+    }
+    
+
+    public static void main(String[] args) {
+        
+    }
+}
